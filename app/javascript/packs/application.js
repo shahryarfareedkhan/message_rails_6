4a32ec8ec86ab096a1fd89ca85ea93339c4c59bf
shahryarfareedkhan <shahryarfreed123@gmail.com>
2022-08-01 19:40:14 +0500

    flash message

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,4 +17,7 @@ ActiveStorage.start()
 
 $(document).on('turbolinks:load', function(){
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+    });    
 })
