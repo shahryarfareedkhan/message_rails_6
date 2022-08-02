86bc641c3e765fff11939d3e30a342b7985edc27
shahryarfareedkhan <shahryarfreed123@gmail.com>
2022-08-02 13:33:26 +0500

    modify display + message scope

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,9 +15,38 @@ Turbolinks.start()
 ActiveStorage.start()
 
 
+
+
 $(document).on('turbolinks:load', function(){
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
         $(this).closest('.message').transition('fade');
-    });    
-})
+    });  
+    ////enter key function 
+    $('#message_body').on('keydown', function(e) {
+        if (e.keyCode == 13) {
+            $('button').click();
+            e.target.value = "";
+        };
+    });
+    ////scroll down function
+    if ($('#messages').length > 0) {
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+});
+
+// scroll_bottom = function() {
+//     if ($('#messages').length > 0) {
+        
+//         $('#messages').scrollTop($('#messages')[0].scrollHeight);
+//     }
+// };
+// submit_message = function() {
+//     $('#message_body').on('keydown', function(e) {
+//         if (e.key == 13) {
+//             alert("hehe");
+//             $('button').click();
+//             e.target.value = "";
+//         };
+//     });
+// };
